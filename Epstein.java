61cebf280bdb549a934b02b691877ab169513fb9
Alice Russell <alice.louise.russell@gmail.com>
2020-06-11 21:18:30 -0400

    comments for class and main method added

--- Epstein.java
+++ Epstein.java
@@ -1,13 +1,25 @@
+/**
+ * Epstein.
+ * 
+ * @author Alice Russell
+ * @version 06/11/2020
+ */
 public class Epstein {
-
+    /**
+     * Main method.
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println(drawBorder());
         System.out.println(drawBorder());
     }
 
+    /**
+     * drawBorder method creates game board border.
+     * @return String. 
+     */
     public static String drawBorder() {
         String border = "+======================+";
         return border;
     }
-
 }
