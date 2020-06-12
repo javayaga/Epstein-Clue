1def1cc99a88a51b6344ba8373c257bc2cf8ded8
Alice Russell <alice.louise.russell@gmail.com>
2020-06-11 21:13:05 -0400

    initial game play class

---
+++ Epstein.java
@@ -0,0 +1,13 @@
+public class Epstein {
+
+    public static void main(String[] args) {
+        System.out.println(drawBorder());
+        System.out.println(drawBorder());
+    }
+
+    public static String drawBorder() {
+        String border = "+======================+";
+        return border;
+    }
+
+}
