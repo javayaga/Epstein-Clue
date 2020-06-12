9df0a3acb1622337ba76d704d1b207c306c572e6
Alice Russell <alice.louise.russell@gmail.com>
2020-06-11 21:34:38 -0400

    drawRooms method

--- Epstein.java
+++ Epstein.java
@@ -10,16 +10,28 @@ public class Epstein {
      * @param args
      */
     public static void main(String[] args) {
+        int [][] rooms = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         System.out.println(drawBorder());
+        System.out.println(drawRooms(rooms));
         System.out.println(drawBorder());
     }
 
     /**
-     * drawBorder method creates game board border.
+     * drawBorder creates game board border.
      * @return String. 
      */
     public static String drawBorder() {
         String border = "+======================+";
         return border;
+    }
+
+    public static String drawRooms(int[][] rooms) {
+        String result = "";
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                result += rooms[i][j];
+            }
+        }
+        return result; 
     }
 }
